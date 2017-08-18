101451f632ebcdc0c1359ac421cec9c2dc5ad44e
jllau <ex_wljr_liujialin@pingan.com.cn>
2017-08-18 18:05:03 +0800

    trieTree

---
+++ trieTree.js
@@ -0,0 +1,60 @@
+var data = [
+  { province: "浙江", city: "杭州", name: "西湖" },
+  { province: "广东", city: "深圳", name: "罗湖" },
+  { province: "广东", city: "广州", name: "白云山" },
+  { province: "广东", city: "清远", name: "石头城" }
+];
+// var transObject = function(tableData, keys) {
+//   let hashTable = {},
+//     res = [];
+//   for (let i = 0; i < tableData.length; i++) {
+//     let arr = res,
+//       cur = hashTable;
+//     for (let j = 0; j < keys.length; j++) {
+//       let key = keys[j],
+//         filed = tableData[i][key];
+//       if (!cur[filed]) {
+//         let pusher = { value: filed },
+//           tmp;
+//         if (j !== keys.length - 1) {
+//           tmp = [];
+//           pusher.children = tmp;
+//         }
+//         cur[filed] = { $$pos: arr.push(pusher) - 1 };
+//         cur = cur[filed];
+//         arr = tmp;
+//       } else {
+//         cur = cur[filed];
+//         arr = arr[cur.$$pos].children;
+//       }
+//     }
+//   }
+//   return res;
+// };
+
+var transObject = function(tableData, keys) {
+  var hashTable = {},
+    res = [];
+  tableData.forEach((tableItem, tableInd, tableArr) => {
+    let arr = res,
+      cur = hashTable;
+    keys.forEach((keyItem, keyInd, keyArr) => {
+      var field = tableItem[keyItem];
+      if (!cur[field]) {
+        let pusher = { value: field },
+          tmp = [];
+        pusher.children = tmp;
+        cur[field] = { $$pos: arr.push(pusher) - 1 };
+        cur = cur[field];
+        arr = tmp;
+      } else {
+        cur = cur[field];
+        arr = arr[cur.$$pos].children;
+      }
+    });
+  });
+  return res;
+};
+
+var keys = ["province", "city", "name"];
+var result = transObject(data, keys);
